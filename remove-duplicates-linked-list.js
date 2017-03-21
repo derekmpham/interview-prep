f284b652d12ccad0c71c68a8b869f2f24a680513
Derek Pham <derekmpham@gmail.com>
2017-03-21 13:32:01 -0700

    Add linked list class to remove duplicates from linked list file

--- remove-duplicates-linked-list.js
+++ remove-duplicates-linked-list.js
@@ -1,5 +1,6 @@
-// create a linked list class
+// Program to remove duplicates from unsorted linked list
 
+// create linked list class
 class LinkedList {
 	constructor() {
 		this.head = null;
@@ -11,11 +12,10 @@ class LinkedList {
 			data: val,
 			next: null
 		};
-		// if linked list is empty
+
 		if (!this.head) {
 			this.head = node;
 		} else {
-			// if not empty
 			var p1 = this.head;
 			while (p1.next) {
 				p1 = p1.next;
@@ -24,18 +24,15 @@ class LinkedList {
 		}
 	}
 
-	// remove node from linked list function
 	deleteNode(val) {
-		// if linked list is empty
 		if (!this.head) {
 			console.log("Linked list is empty");
 			return;
 		}
-		// if trying to remove head from linked list
+
 		if (this.head.data === val) {
 			this.head = this.head.next;
 		} else {
-			// removing any other node from linked list
 			var p1 = this.head;
 			var p2 = p1.next;
 			while (p2) {
