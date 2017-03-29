291e6ac00a6a1b2c6170ea2097daa8601c4923c8
Derek Pham <derekmpham@gmail.com>
2017-03-28 23:40:08 -0700

    Add quicksort function file

---
+++ quick-sort.js
@@ -0,0 +1,14 @@
+function quicksort(arr) {
+	if(arr.length == 0) {
+		return [];
+	}
+	var left = [], right = [], pivot = arr[0];
+	for(var i = 1; i < arr.length; i++) {
+		if(arr[i] < pivot) {
+			left.push(arr[i]);
+		} else {
+			right.push(arr[i]);
+		}
+	}
+	return quicksort(left).concat(pivot, quicksort(right));
+}
