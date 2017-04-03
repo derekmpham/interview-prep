00642cb34569c9ca324b11637b83ea53f5c34216
Derek Pham <derekmpham@gmail.com>
2017-04-02 17:12:27 -0700

    Create node in binary search tree program

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,1 +1,8 @@
 // Program that creates a binary search tree: each node has up to two children, and all left descendants of a node is less than or equal to the node and all right descendants are greater than the node
+
+// create node
+function Node(val) {
+	this.value = val;
+	this.left = null;
+	this.right = null;
+}
