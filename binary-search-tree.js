f3813bcc42ddd37962a56d3d1d196a608e0437e4
Derek Pham <derekmpham@gmail.com>
2017-04-02 17:11:54 -0700

    Begin writing program for creating binary search tree

---
+++ binary-search-tree.js
@@ -0,0 +1,1 @@
+// Program that creates a binary search tree: each node has up to two children, and all left descendants of a node is less than or equal to the node and all right descendants are greater than the node
