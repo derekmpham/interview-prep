7003358ac2b8f1fab7c3bd720d5b4166cdbcd0d2
Derek Pham <derekmpham@gmail.com>
2017-04-02 17:18:11 -0700

    Create constructor for binary search tree

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,8 +1,13 @@
 // Program that creates a binary search tree: each node has up to two children, and all left descendants of a node is less than or equal to the node and all right descendants are greater than the node
 
-// create node
+// create node class
 function Node(val) {
 	this.value = val;
 	this.left = null;
 	this.right = null;
+}
+
+// create constructor for binary search tree
+function BinarySearchTree() {
+	this.root = null;
 }
