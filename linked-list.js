6d59775bab32493d7fb24df3acce5294a8940922
Derek Pham <derekmpham@gmail.com>
2017-05-04 12:36:26 -0700

    Add add-function to linked list

--- linked-list.js
+++ linked-list.js
@@ -87,3 +87,28 @@ function LinkedList() {
 	this._length = 0; // assign number of nodes in linked list
 	this.head = null; // points to head of linked list (node at front of linked list)
 }
+
+// add node to linked list
+LinkedList.prototype.add = function(val) {
+	var node = new Node(val), // create new instance of node
+		currentNode = this.head;
+
+	// first case: if linked list is initially empty
+	if (!currentNode) {
+		this.head = node; // make new node head of linked list
+		this._length++;
+
+		return node;
+	}
+
+	// second case: if list linked is initially not empty
+	while (currentNode.next) {
+		currentNode = currentNode.next; // iterate through entire non-empty linked list to get to end of linked list
+	}
+
+	currentNode.next = node; // add new node to end of linked list
+
+	this._length++;
+
+	return node;
+};
