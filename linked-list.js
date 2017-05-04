c4c6cf4cc5255c120f85a420ad3f03c3558ec94f
Derek Pham <derekmpham@gmail.com>
2017-05-04 15:20:39 -0700

    Add test cases to linked list implementation

--- linked-list.js
+++ linked-list.js
@@ -1,81 +1,6 @@
 "use strict";
 
-// Create a linked list class
-
-class LinkedList {
-	constructor() {
-		this.head = null;
-	}
-
-	// add node to linked list function
-	addNodeToTail(val) {
-		var node = {
-			data: val,
-			next: null
-		};
-		// if linked list is empty
-		if (!this.head) {
-			this.head = node;
-		} else {
-			// if not empty
-			var p1 = this.head;
-			while (p1.next) {
-				p1 = p1.next;
-			}
-			p1.next = node;
-		}
-	}
-
-	// remove node from linked list function
-	deleteNode(val) {
-		// if linked list is empty
-		if (!this.head) {
-			console.log("Linked list is empty");
-			return;
-		}
-		// if trying to remove head from linked list
-		if (this.head.data === val) {
-			this.head = this.head.next;
-		} else {
-			// removing any other node from linked list
-			var p1 = this.head;
-			var p2 = p1.next;
-			while (p2) {
-				if (p2.data === val) {
-					p1.next = p2.next;
-					break;
-				} else {
-					p1 = p2;
-				}
-				p2 = p2.next;
-			}
-		}
-	}
-}
-
-
-// test cases
-// create instance of LinkedList class
-var listOne = new LinkedList();
-
-// create linked list with 5 nodes
-listOne.addNodeToTail(1);
-listOne.addNodeToTail(2);
-listOne.addNodeToTail(3);
-listOne.addNodeToTail(4);
-listOne.addNodeToTail(5);
-console.log(listOne);
-
-// delete head and tail nodes
-listOne.deleteNode(1);
-listOne.deleteNode(5);
-// expect linked list to have 3 nodes
-console.log(listOne);
-
-// delete node in middle of linked list
-listOne.deleteNode(3);
-console.log(listOne);
-
+// LINKED LIST
 
 // define constructor
 function Node(data) {
@@ -174,3 +99,17 @@ LinkedList.prototype.remove = function(position) {
 
 	return deleteNode;
 };
+
+// test cases
+var test = new LinkedList();
+test.add(6);
+test.add(7);
+test.add(8);
+test.add(9);
+test.add(10);
+console.log(test); // expect linked list with five nodes
+
+test.searchNodeAt(5); // expect node with value 10 to be returned
+
+test.remove(5); // remove node at position 5
+test.searchNodeAt(5); // expect error message after removing node
