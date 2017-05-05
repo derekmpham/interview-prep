9959cfcf64f6e6f8ea5a6fa638578e5cde8a3033
Derek Pham <derekmpham@gmail.com>
2017-05-04 19:26:38 -0700

    Add add-method to doubly linked list

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -15,3 +15,20 @@ function DoublyLinkedList() {
 	this.tail = null;
 }
 
+// add node to doubly linked list
+DoublyLinkedList.prototype.add = function(val) {
+	var node = new Node(val); // create new instance of node
+
+	if(this._length) {
+		this.tail.next = node;
+		node.previous = this.tail; // account for bi-directional movement
+		this.tail = node; // new node now tail
+	} else { // if doubly linked list is empty
+		this.head = node;
+		this.tail = node;
+	}
+
+	this._length++;
+
+	return node;
+}
