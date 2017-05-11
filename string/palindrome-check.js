7e90e8e0bcfbe60c4682a08d21a4aa95c5ffa465
Derek Pham <derekmpham@gmail.com>
2017-05-11 16:46:03 -0700

    Add regex to check palindrome algorithm

---
+++ string/palindrome-check.js
@@ -0,0 +1,9 @@
+// Program to determine if string is a palindrome
+
+function checkPalindrome(str) {
+
+	// remove all non-alphanumeric characters through regular expression
+	var regEx = /[^A-Za-z0-9]/g;
+
+	
+}
