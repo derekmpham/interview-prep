a6a1f69ef835325f8a785c8f4ab741fc003258f2
Derek Pham <derekmpham@gmail.com>
2017-05-11 17:05:24 -0700

    Add check-for-palindrome loop

--- string/palindrome-check.js
+++ string/palindrome-check.js
@@ -7,4 +7,12 @@ function checkPalindrome(str) {
 
 	// lower case all characters in string and remove all non-alphanumeric characters
 	var lowCaseReg = str.toLowerCase().replace(regEx, '');
+
+	// check for palindrome with loop
+	for (var i = 0; i < (str.length)/2; i++) {
+		if (str[i] !== str[(str.length) - 1 - i]) { // loop will go on as long as characters match
+			return false;
+		}
+	}
+	return true; // string is palindrome
 }
