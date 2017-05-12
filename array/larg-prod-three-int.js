73af159de5f434b4c6d5bbfc6017791bfe5a576c
Derek Pham <derekmpham@gmail.com>
2017-05-12 12:13:17 -0700

    Begin writing largest product function and establish variables needed

--- array/larg-prod-three-int.js
+++ array/larg-prod-three-int.js
@@ -3,3 +3,10 @@
 function sortIntegers(a, b) {
 	return a - b;
 }
+
+function largestProduct(unsortedArray) {
+	var sortedArray = unsortedArray.sort(sortIntegers),
+		productOne = 1,
+		productTwo = 1,
+		index = sortedArray.length - 1;
+}
