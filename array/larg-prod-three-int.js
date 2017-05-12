4edf97181786d9be35791cbc46276fc2d29118a7
Derek Pham <derekmpham@gmail.com>
2017-05-12 14:13:37 -0700

    Get product of three largest integers in sorted array

--- array/larg-prod-three-int.js
+++ array/larg-prod-three-int.js
@@ -8,5 +8,10 @@ function largestProduct(unsortedArray) {
 	var sortedArray = unsortedArray.sort(sortIntegers),
 		productOne = 1,
 		productTwo = 1,
-		index = sortedArray.length - 1;
+		lastElementIndex = sortedArray.length - 1;
+
+	// get product of three largest integers in sorted array
+	for (var i = lastElementIndex; i > lastElementIndex - 3; i--) {
+		productOne = productOne * sortedArray[i];
+	}
 }
