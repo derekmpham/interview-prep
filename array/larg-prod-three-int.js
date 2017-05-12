be1cb6076da0ee597bb11fd82bb6461b3c154012
Derek Pham <derekmpham@gmail.com>
2017-05-12 14:48:45 -0700

    Add test case

--- array/larg-prod-three-int.js
+++ array/larg-prod-three-int.js
@@ -4,7 +4,7 @@ function sortIntegers(a, b) {
 	return a - b;
 }
 
-// greatest product either min1 * min2 * max1 OR max1 * max2 * max3
+// largest product yield either max1 * max2 * max3 OR min1 * min2 * max1
 function largestProduct(unsortedArray) {
 	var sortedArray = unsortedArray.sort(sortIntegers),
 		productOne = 1,
@@ -13,14 +13,19 @@ function largestProduct(unsortedArray) {
 
 	// get product of three largest integers in sorted array
 	for (var i = lastElementIndex; i > lastElementIndex - 3; i--) {
-		productOne = productOne * sortedArray[i];
+		productOne = productOne * sortedArray[i]; // first case: max1 * max2 * max3
 	}
 
+	// second case: min1 * min2 * max1
 	productTwo = sortedArray[0] * sortedArray[1] * sortedArray[lastElementIndex];
 
+	// compare first and second cases and return larger product yield
 	if (productOne > productTwo) {
 		return productOne;
 	}
 
 	return productTwo;
 }
+
+// test case
+largestProduct([-10, 7, 29, 30, 5, -10, -70]); // expect to return 21000
