1b273af40df9a093271b2d21cebcc545f2f55dce
Derek Pham <derekmpham@gmail.com>
2017-05-11 17:16:47 -0700

    Add test cases to check for palindrome in string function

--- string/palindrome-check.js
+++ string/palindrome-check.js
@@ -9,10 +9,18 @@ function checkPalindrome(str) {
 	var lowCaseReg = str.toLowerCase().replace(regEx, '');
 
 	// check for palindrome with loop
-	for (var i = 0; i < (str.length)/2; i++) {
-		if (str[i] !== str[(str.length) - 1 - i]) { // loop will go on as long as characters match
+	for (var i = 0; i < (lowCaseReg.length)/2; i++) {
+		if (lowCaseReg[i] !== lowCaseReg[(lowCaseReg.length) - 1 - i]) { // loop will go on as long as characters match
 			return false;
 		}
 	}
 	return true; // string is palindrome
 }
+
+// test cases
+checkPalindrome("racecar"); // expect true
+checkPalindrome("race car"); // expect true
+checkPalindrome("nope"); // expect false
+checkPalindrome("not palindrome"); // expect false
+checkPalindrome("A man, a plan, a canal. Panama"); // expect true
+checkPalindrome("0_0 (: /-\ :) 0_0"); // expect true
