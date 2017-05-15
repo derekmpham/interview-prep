8bcd6c17a3604bc62a6c8996b285447eef700db6
Derek Pham <derekmpham@gmail.com>
2017-05-15 12:15:42 -0700

    Add return unique elements array function

--- array/unique-elements.js
+++ array/unique-elements.js
@@ -1,7 +1,15 @@
 // Program to remove duplicates of array and return array of only unique elements
 
 function uniqueElements(arr) {
-	var hashmap = {};
-	var unique = [];
+	var hashmap = {}; // to be used as reference table
+	var unique = []; // array to be returned
 
+	for (var i = 0; i < arr.length; i++) {
+		// if key returns undefined (unique), it is evaluated as false
+		if (!hashmap.hasOwnProperty(arr[i])) { // if element does not exist in hash
+			hashmap[arr[i]] = 1; // add element to hash table as key (and assign random number 1 as value)
+			unique.push(arr[i]); // push element into unique array
+		}
+	}
+	return unique;
 }
