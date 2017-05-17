2569ab19ab1ae516451a8018b8911fd82c72e100
Derek Pham <derekmpham@gmail.com>
2017-05-17 16:43:57 -0700

    Use hashmap's O(1) look up time to check if an element in second array exists in the hash (i.e. first array), iterating through entire second array

--- array/common-elements-two-arrays.js
+++ array/common-elements-two-arrays.js
@@ -8,6 +8,13 @@ function intersection(firstArray, secondArray) {
 	arrOne.forEach(function(element) {
 		hashMap[element] = 1;
 	})
+
+	// use hashmap's O(1) look up time to check if an element in second array exists in the hash (i.e. first array)
+	secondArray.forEach(function(element) { // iterate through entire second array
+		if (hashMap[element] === 1) { // if element in second array exists in hashmap
+			commonElements.push(element); // add matching element into output array
+		}
+	});
 }
 
 
