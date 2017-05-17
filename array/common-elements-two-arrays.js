5bafb1452dc049d242e0d7c8b6509c0e875c08b9
Derek Pham <derekmpham@gmail.com>
2017-05-17 16:16:18 -0700

    Begin writing algorithm for finding intersection of two arrays

---
+++ array/common-elements-two-arrays.js
@@ -0,0 +1,10 @@
+// Program that identifies common element(s) that exist within both two arrays
+
+function intersection(firstArray, secondArray) {
+	var hashMap = {};
+	var commonElements = [];
+
+
+}
+
+
