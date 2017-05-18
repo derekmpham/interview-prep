fd743883ac7f1d11671dfeb04a454fda2f1a07ef
Derek Pham <derekmpham@gmail.com>
2017-05-18 11:02:58 -0700

    Add test case

--- string/reverse.js
+++ string/reverse.js
@@ -1,5 +1,13 @@
-// Program to reverse sentence (each word and entirely)
+// Program to reverse each word in a sentence
 
-function reverse(str, separator) {
+function reverseBySeparator(str, separator) {
 	return str.split(separator).reverse().join(separator);
 }
+
+// test case
+var testString = "Hey dude!";
+// reverse entire sentence
+var reverseSentence = reverseBySeparator(testString, ""); // expect to return !edud yeH
+
+// reverse each word
+reverseBySeparator(reverseSentence, " "); // expect to return yeH !edud
