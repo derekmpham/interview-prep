3cd35eafa8b20f909ae37c2c8b81659716566a27
Derek Pham <derekmpham@gmail.com>
2017-05-19 11:20:21 -0700

    Begin writing algorithm for identifying anagrams: lower case both strings

---
+++ string/anagrams.js
@@ -0,0 +1,7 @@
+// Program that returns true if two strings are anagrams of one another
+
+function isAnagram(firstStr, secondStr) {
+	// lower case both strings to account for case insensitivity
+	var a = firstStr.toLowerCase(),
+		b = secondStr.toLowerCase();
+}
