fd4acac45a2c0843368c3e18841531f5c4e8a04f
Derek Pham <derekmpham@gmail.com>
2017-05-19 11:23:33 -0700

    Sort strings and join each resulting array to a string

--- string/anagrams.js
+++ string/anagrams.js
@@ -4,4 +4,8 @@ function isAnagram(firstStr, secondStr) {
 	// lower case both strings to account for case insensitivity
 	var a = firstStr.toLowerCase(),
 		b = secondStr.toLowerCase();
+
+	// sort strings and join each resulting array to a string
+	a = a.split("").sort().join("");
+	b = b.split("").sort().join("");
 }
