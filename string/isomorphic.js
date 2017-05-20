6e287e1524e7f20a40a52d571c0ee15d88a9d62d
Derek Pham <derekmpham@gmail.com>
2017-05-20 09:42:21 -0700

    Create loop and set first condition: if letter does not exist, create map and map it to value of second letter

--- string/isomorphic.js
+++ string/isomorphic.js
@@ -2,9 +2,36 @@
 
 function isIsomorphic(stringOne, stringTwo) {
 	
-	// check if strings are of the same length
+	// check if strings are of the same length (if not, cannot be isomorphic)
 	if (stringOne.length !== stringTwo.length) {
 		return false;
 	}
 
+	var letterMap = {};
+
+	for (i = 0; i < stringOne.length; i++) {
+		var letterA = stringOne[i],
+			letterB = stringTwo[i];
+
+		// if the letter does not exist, create a map and map it to the value of the second letter
+		if (letterMap[letterA] === undefined) {
+			letterMap[letterA] = letterB;
+		}
+	}
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
