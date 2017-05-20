a12374761331564853ce4675975bc47de47441c0
Derek Pham <derekmpham@gmail.com>
2017-05-20 09:35:30 -0700

    Begin writing isomorphic check aalgorithm: check if strings are of the same length

---
+++ string/isomorphic.js
@@ -0,0 +1,10 @@
+// Check if two strings are isomorphic
+
+function isIsomorphic(stringOne, stringTwo) {
+	
+	// check if strings are of the same length
+	if (stringOne.length !== stringTwo.length) {
+		return false;
+	}
+
+}
