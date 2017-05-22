33ae83b6abe0fd805b08a1e2e681db54db0594b2
Derek Pham <derekmpham@gmail.com>
2017-05-22 10:50:02 -0700

    Set up other two scenarios: implement recursion for each

--- recursion-binary-search.js
+++ recursion-binary-search.js
@@ -7,9 +7,13 @@ function binarySearch(arr, val, leftPosition, rightPosition) {
 		return -1;
 	}
 
+	// if value exists
 	var middlePivot = Math.floor((leftPosition + rightPosition) / 2);
 	if (arr[middlePivot] === val) {
 		return middlePivot;
+	} else if (arr[middlePivot] > value) {
+		return binarySearch(arr, val, leftPosition, middlePivot - 1); // implement recursion
+	} else {
+		return binarySearch(arr, val, middlePivot + 1, rightPosition); // implement recursion
 	}
-
 }
