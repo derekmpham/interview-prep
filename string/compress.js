bc77bdfb64478cc2544bebe59197c9aa30043c74
Derek Pham <derekmpham@gmail.com>
2017-05-27 19:01:45 -0700

    Begin writing string compression algorithm: set up basic variables

---
+++ string/compress.js
@@ -0,0 +1,8 @@
+function compressStr(str) {
+	var output = "", // will return this variable as final compressed string
+		currChar = "", // represents current character being looked at
+		currCount = "", // counts number of times character is repeated
+		maxCount = ""; // maximum count
+
+
+}
