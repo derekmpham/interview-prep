745fc895b8d1d07f4da1dd3066220a833607ece6
Derek Pham <derekmpham@gmail.com>
2017-06-09 09:55:09 -0700

    Add merge function

--- array/merge-sort.js
+++ array/merge-sort.js
@@ -1,6 +1,5 @@
 // Implement merge sort: "divide and conquer" algorithm using recursion
 
-
 function mergeSort(arr) {
 	if (arr.length < 2) {
 		return arr;
@@ -11,4 +10,26 @@ function mergeSort(arr) {
 		right = arr.slice(middle, arr.length);
 
 	return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+	var result = [];
+
+	while (left.length && right.length) {
+		if (left[0] <= right[0]) {
+			result.push(left.shift());
+		} else {
+			result.push(right.shift());
+		}
+	}
+
+	while (left.length) {
+		result.push(left.shift());
+	}
+
+	while (right.length) {
+		result.push(right.shift());
+	}
+
+	return result;
 }
