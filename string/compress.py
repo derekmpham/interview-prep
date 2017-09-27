61b38528b60203003b9595f7ba2204c287dc6970
Derek Pham <derekmpham@gmail.com>
2017-09-27 10:58:25 -0700

    Add to current count if there is a match

--- string/compress.py
+++ string/compress.py
@@ -11,3 +11,5 @@ def compress_str(str):
 			output = output + curr_char + char_count # add new unique character and its count to our output
 			curr_char = str[i] # move on to the next character in string
 			char_count = 1 # reset count to 1
+		else: # add to repeated count if there is a match
+			char_count += 1
