b2f6d9c5fec4316dbceb25bd91cb5831065c1957
Derek Pham <derekmpham@gmail.com>
2017-09-27 15:57:35 -0700

    Rewrite method and add test cases

--- string/compress.py
+++ string/compress.py
@@ -1,15 +1,25 @@
 # Compress string using counts of repeated characters
 
-def compress_str(str):
+def compress_str(string):
 	output = ""
-	curr_char = ""
-	char_count = ""
+	char_count = 1
 
-	for i in str:
+	# add first character
+	output += string[0]
 
-		if curr_char != str[i]:
-			output = output + curr_char + char_count # add new unique character and its count to our output
-			curr_char = str[i] # move on to the next character in string
-			char_count = 1 # reset count to 1
-		else: # add to repeated count if there is a match
+	for i in range(len(string)-1): # doesn't do character count for last group of a unique character
+		if string[i] == string[i+1]:
 			char_count += 1
+		else:
+			output += str(char_count) + string[i+1] # add count and next unique character
+			char_count = 1 # reset character count
+
+	# account for count of last character
+	output += str(char_count)
+
+	print output
+
+
+# test cases
+compress_str("ssssssss") # prints "s8"
+compress_str("abbcccdddd") # prints "a1b2c3d4"
