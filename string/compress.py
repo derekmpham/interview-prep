41605f37e60d0975c34cfaf615ee86a65aa1b711
Derek Pham <derekmpham@gmail.com>
2017-09-27 10:57:30 -0700

    Elaborate what to do if we find a new unique character in string

--- string/compress.py
+++ string/compress.py
@@ -3,4 +3,11 @@
 def compress_str(str):
 	output = ""
 	curr_char = ""
-	curr_count = ""
+	char_count = ""
+
+	for i in str:
+
+		if curr_char != str[i]:
+			output = output + curr_char + char_count # add new unique character and its count to our output
+			curr_char = str[i] # move on to the next character in string
+			char_count = 1 # reset count to 1
