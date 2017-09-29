6b74fad80ca9f011cb012d2f9a19f96409b77c48
Derek Pham <derekmpham@gmail.com>
2017-09-29 14:43:06 -0700

    Compare first element in list to the rest

--- array/quick-sort.py
+++ array/quick-sort.py
@@ -4,6 +4,15 @@ def quick_sort(lst):
 	if len(lst) == 0:
 		print []
 
+	left = []
+	right = []
+	pivot = lst[0]
 
+	# compare first element in list to the rest
+	for i in range(1, len(lst)):
+		if lst[i] < pivot:
+			left.append(lst[i])
+		else:
+			right.append(lst[i])
 
 
