fe41aabf073ce3a02b5af117120d62ffc0324655
Derek Pham <derekmpham@gmail.com>
2017-09-30 15:16:59 -0700

    Add search method for python linked list implementation

--- linked-list/linked-list.py
+++ linked-list/linked-list.py
@@ -18,3 +18,26 @@ class LinkedList(object):
 			current_node.next = new_node # add to end of linked list
 		else:
 			self.head = new_node
+
+	def search(self, position):
+		counter = 1
+		current_node = self.head
+		if position < 1:
+			return None
+		while current_node and counter <= position: # go through linked list until you reach input position
+			if counter == position:
+				return current_node
+			current_node = current_node.next
+			counter += 1
+		return None # if position larger than length of linked list
+
+
+
+
+
+
+
+
+
+
+
