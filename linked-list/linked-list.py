25b9b4ea5ae8367d73f8ea8693d1d15a731dbd09
Derek Pham <derekmpham@gmail.com>
2017-09-30 15:05:00 -0700

    Incorporate add node method to linked list python implementation

--- linked-list/linked-list.py
+++ linked-list/linked-list.py
@@ -10,4 +10,11 @@ class LinkedList(object):
 	def __init__(self, head=None):
 		self.head = head
 
-
+	def add(self, new_node):
+		current_node = self.head
+		if self.head:
+			while current_node.next:
+				current_node = current_node.next
+			current_node.next = new_node # add to end of linked list
+		else:
+			self.head = new_node
