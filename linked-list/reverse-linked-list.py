254ea51140c1c7ede701f4844bc8c297edabab84
Derek Pham <derekmpham@gmail.com>
2017-09-30 20:07:16 -0700

    Add linked list class

--- linked-list/reverse-linked-list.py
+++ linked-list/reverse-linked-list.py
@@ -4,3 +4,7 @@ class Node(object): # define constructor
 	def __init__(self, data):
 		self.data = data
 		self.next = None
+
+class LinkedList(object):
+	def __init__(self, head=None):
+		self.head = head
