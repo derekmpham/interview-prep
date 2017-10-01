592add36a97742f8dcace17b4cf91474f4a2dc87
Derek Pham <derekmpham@gmail.com>
2017-10-01 10:34:28 -0700

    Add remove first node method to linked list that will be used for pop method for stack

--- stack/stack.py
+++ stack/stack.py
@@ -10,6 +10,15 @@ class LinkedList(object):
 	def __init__(self, head=None):
 		self.head = head
 
-	def insert_first(self, new_node):
+	def insert_first(self, new_node): # pushing on the first node runs much faster than going through entire list and adding new node to the end/tail (i.e. add method in original linked list method)
 		new_node.next = self.head
 		self.head = new_node
+
+	def remove_first(self): # popping on first node runs much faster than going through entire list and removing end/tail node
+		if self.head:
+			deleted_node = self.head
+			temp = deleted_node.next
+			self.head = temp
+			return deleted_node
+		else:
+			return None
