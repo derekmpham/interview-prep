aad3b53318335153bdec99921ef6d4840abb3f84
Derek Pham <derekmpham@gmail.com>
2017-10-01 10:17:19 -0700

    Add initialize method to linked list class

--- stack/stack.py
+++ stack/stack.py
@@ -5,3 +5,7 @@ class Node(object): # define constructor
 	def __init__(self, data):
 		self.data = data
 		self.next = None
+
+class LinkedList(object):
+	def __init__(self, head=None):
+		self.head = head
