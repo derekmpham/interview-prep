bae23ed8463e259bb34ed862511cc0225f37fa9a
Derek Pham <derekmpham@gmail.com>
2017-10-01 21:16:48 -0700

    Add test case

--- array/create-queue.py
+++ array/create-queue.py
@@ -12,3 +12,18 @@ class Queue:
 
 	def peek(self): # take a look at the first in line
 		return self.storage[0]
+
+
+# test case
+test_queue = Queue(1)
+test_queue.enqueue(2)
+test_queue.enqueue(3)
+
+print test_queue.peek() # prints 1
+print test_queue.dequeue() # prints 1
+test_queue.enqueue(4)
+print test_queue.dequeue() # prints 2
+print test_queue.dequeue() # prints 3
+print test_queue.dequeue() # prints 4
+test_queue.enqueue(5)
+print test_queue.peek() # prints 5
