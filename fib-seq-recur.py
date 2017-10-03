53972cb11ae4825b2ae7b59b2a31d2d89d253cb5
Derek Pham <derekmpham@gmail.com>
2017-10-02 17:32:01 -0700

    Add test cases

--- fib-seq-recur.py
+++ fib-seq-recur.py
@@ -5,3 +5,9 @@ def get_fib(position):
 		return position
 	else:
 		return get_fib(position-1) + get_fib(position-2) # add two previous numbers
+
+
+# test cases
+print get_fib(9) # returns 34
+print get_fib(11) # returns 89
+print get_fib(0) # returns 0
