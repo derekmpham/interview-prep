6258e75e6c017508f614e1c9bd1e758adf9f8e07
Derek Pham <derekmpham@gmail.com>
2017-10-05 11:28:05 -0700

    Add check process

--- array/keywords-search.py
+++ array/keywords-search.py
@@ -5,4 +5,19 @@ def find_adjacent(lst, target):
 
 	# create set for check process
 	s = {key for key in target}
-	print s
+	# print s
+
+	# check process: find two elements in sequence
+	indices = []
+	i = 0
+	while i < len(lst):
+		if lst[i] in s:
+			j = i # j "records" position
+			while j < len(lst) and lst[j] in s:
+				j += 1
+			if j - i > 1:
+				indices.append(i + 1) # +1 because we are looking for the first index
+			i = j
+		else:
+			i += 1
+	print indices
