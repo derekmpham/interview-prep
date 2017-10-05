c12ca0b6ff9aba67bf71fdab65cf36fcb70cba0d
Derek Pham <derekmpham@gmail.com>
2017-10-05 14:03:08 -0700

    Add store string method

--- hashtable.py
+++ hashtable.py
@@ -3,3 +3,11 @@
 class HashTable(object):
 	def __init__(self):
 		self.table = [None]*10000
+
+	def store(self, string):
+		hash_value = self.calculate_hash_value(string)
+		if hash_value != -1:
+			if self.table[hash_value] != None:
+				self.table[hash_value].append(string)
+			else:
+				self.table[hash_value] = [string]
