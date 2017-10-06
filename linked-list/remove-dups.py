0ac19b362f8675197ac06c05bafc6d67f6c5db89
Derek Pham <derekmpham@gmail.com>
2017-10-06 11:57:09 -0700

    Add search method

--- linked-list/remove-dups.py
+++ linked-list/remove-dups.py
@@ -16,4 +16,16 @@ class LinkedList(object):
 				current_node = current_node.next
 				current_node.next = new_node # add to end of linked list
 
-	
+	def search(self, position):
+		counter = 1
+		current_node = self.head
+		if position < 1:
+			return None
+		while current_node and counter <= position: # go through linked list until you reach input position
+			if counter == position:
+				return current_node
+			current_node = current_node.next
+			counter += 1
+		return None # if position is larger than length of linked list
+
+
