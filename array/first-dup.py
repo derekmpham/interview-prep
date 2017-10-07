c4d73b3fea7b890c1568a11ffea698f3ecbb019b
Derek Pham <derekmpham@gmail.com>
2017-10-07 11:12:07 -0700

    Add test cases

--- array/first-dup.py
+++ array/first-dup.py
@@ -9,3 +9,17 @@ def first_duplicate(a):
 		else:
 			return a[i]
 	return -1
+
+
+# test cases
+print first_duplicate([2, 3, 3, 1, 5, 2]) # prints 3
+print first_duplicate([2, 4, 3, 5, 1]) # prints -1
+print first_duplicate([1]) # prints -1
+print first_duplicate([2, 2]) # prints 2
+print first_duplicate([2, 1]) # prints -1
+print first_duplicate([2, 1, 3]) # prints -1
+print first_duplicate([2, 3, 3]) # prints 3
+print first_duplicate([3, 3, 3]) # prints 3
+print first_duplicate([8, 4, 6, 2, 6, 4, 7, 9, 5, 8]) # prints 6
+print first_duplicate([10, 6, 8, 4, 9, 1, 7, 2, 5, 3]) # prints -1
+print first_duplicate([1, 1, 2, 2, 1]) # prints 1
