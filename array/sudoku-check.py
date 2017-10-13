ca9c4f7c3f1f7690395948b9dcdfd917cc33bfa8
Derek Pham <derekmpham@gmail.com>
2017-10-12 21:20:07 -0700

    Add create sub grid method

--- array/sudoku-check.py
+++ array/sudoku-check.py
@@ -37,3 +37,18 @@ def create_sub_grid(grid):
 		else:
 			ref_check[square] = 1
 	return True
+
+def check_sub_grid(grid):
+	sub_grid = []
+	for row in range(0, 9, 3):
+		for i in range(0, 9, 3):
+			a = []
+			for j in range(row, row + 3):
+				for column in range(i, i + 3):
+					a.append(grid[j][column])
+					sub_grid.append(a)
+
+	for row in sub_grid:
+		if not create_sub_grid(row):
+			return False
+	return True
