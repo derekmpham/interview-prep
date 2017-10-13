550077b942fb203aa1d2d679dc13ef07561982d3
Derek Pham <derekmpham@gmail.com>
2017-10-13 16:02:02 -0700

    Refactor check rows method

--- array/sudoku-check.py
+++ array/sudoku-check.py
@@ -1,18 +1,14 @@
 # Implement an algorithm that will check whether a given grid of numbers represents a valid Sudoku puzzle
 
 def check_rows(grid):
-	i = 0
-	while i < len(grid):
-		j = 0
+	for i in range(9):
 		ref_check = {}
-		while j < len(grid[i]):
+		for j in range(9):
 			if grid[i][j] != '.' and grid[i][j] in ref_check:
 				return False
 			else:
 				ref_check[grid[i][j]] = 1
-				j += 1
-			i += 1
-		return True
+	return True
 
 def check_columns(grid):
 	column = 0
