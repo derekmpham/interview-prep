d5587c01d2bd1d07e6f371916411b2d95e13008d
Derek Pham <derekmpham@gmail.com>
2017-10-18 21:36:30 -0700

    Add create nodes methods and test cases

--- linked-list/remove-k-from-list.py
+++ linked-list/remove-k-from-list.py
@@ -5,19 +5,6 @@ class Node(object): # define constructor
 		self.value = value
 		self.next = None
 
-class LinkedList(object):
-	def __init__(self, head=None):
-		self.head = head
-
-	def add(self, new_node):
-		current_node = self.head
-		if self.head:
-			while current_node.next:
-				current_node = current_node.next
-			current_node.next = new_node # add to end of linked list
-		else:
-			self.head = new_node
-
 def remove_k_from_list(l, k):
 	fake_head = Node(None)
 	fake_head.next = l
@@ -28,3 +15,29 @@ def remove_k_from_list(l, k):
 			current_node.next = current_node.next.next
 		current_node = current_node.next
 	return fake_head.next
+
+def create_nodes(l):
+	root = Node(-1)
+	current_node = root
+	for value in l:
+		current_node.next = Node(value)
+		current_node = current_node.next
+	return root.next
+
+
+# test cases
+list_one = [3, 1, 2, 3, 4, 5]
+k_one = 3
+filtered_list_one = remove_k_from_list(create_nodes(list_one), k_one)
+print filtered_list_one.value # prints 1
+
+list_two = [1, 2, 3, 4, 5, 6, 7]
+k_two = 10
+filtered_list_two = remove_k_from_list(create_nodes(list_two), k_two)
+print filtered_list_two.value # prints 4
+print filtered_list_two.next.next.next.next.next.next.value # prints 7
+
+list_three = [1000, 1000]
+k_three = 1000
+filtered_list_three = remove_k_from_list(create_nodes(list_three), k_three)
+print filtered_list_three # prints None
