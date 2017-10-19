6557cbe8bee7ded848ba7c3928e2b4f82aedeea8
Derek Pham <derekmpham@gmail.com>
2017-10-18 20:43:52 -0700

    Add linked list class and add method

--- linked-list/remove-k-from-list.py
+++ linked-list/remove-k-from-list.py
@@ -1,9 +1,22 @@
 # Given a singly linked list of integers l and an integer k, remove all elements from list l that have a value equal to k
 
-class Node(object):
+class Node(object): # define constructor
 	def __init__(self, value):
 		self.value = value
 		self.next = None
+
+class LinkedList(object):
+	def __init__(self, head=None):
+		self.head = head
+
+	def add(self, new_node):
+		current_node = self.head
+		if self.head:
+			while current_node.next:
+				current_node = current_node.next
+			current_node.next = new_node # add to end of linked list
+		else:
+			self.head = new_node
 
 def remove_k_from_list(l, k):
 	fake_head = Node(None)
