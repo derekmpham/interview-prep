01ae7d18a8f553c8b6cb0988449b22149ca5acb5
Derek Pham <derekmpham@gmail.com>
2017-10-19 21:22:02 -0700

    Begin writing algorithm for identifying if linked list is palindrome: add node constructor

---
+++ linked-list/is-list-palindrome.py
@@ -0,0 +1,5 @@
+# Given a singly linked list of integers, determine whether or not it's a palindrome
+class Node(object): # define constructor
+	def __init__(self, value):
+		self.value = value
+		self.next = None
