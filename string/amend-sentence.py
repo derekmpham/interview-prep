a29356419aeb23a242fcd8a66169ed9950d3781e
Derek Pham <derekmpham@gmail.com>
2017-10-27 12:33:22 -0700

    Add amend sentence algorithm

---
+++ string/amend-sentence.py
@@ -0,0 +1,14 @@
+# You have been given a string s, which is supposed to be a sentence. However, someone forgot to put spaces between the different words, and for some reason they capitalized the first letter of every word. Return the sentence after making the following amendments: (1) Put a single space between the words; and (2) Convert the uppercase letters to lowercase
+
+def amend_sentence(s):
+	sentence = ""
+
+	for i, char in enumerate(s):
+		if char.isupper():
+			if i != 0: # add space before upper case letter (that is not first letter of input string) and lowercase that letter
+				sentence += " " + char.lower()
+			else:
+				sentence += char.lower()
+		else: # add already lowercased letter to output sentence
+			sentence += char
+	return sentence
