9262e9aba531987362ed64e056563c660a33a50d
Derek Pham <derekmpham@gmail.com>
2017-11-19 17:30:20 -0800

    Begin first string substring  occurence algorithm: store lengths of string and substring

---
+++ string/first-str-substr-occr.py
@@ -0,0 +1,5 @@
+# Implement a function that takes two strings, s and x, as arguments and finds the first occurrence of the string x in s. The function should return an integer indicating the index in s of the first occurrence of x. If there are no occurrences of x in s, return -1
+
+def find_substring(string, substr):
+	string_len = len(string)
+	substr_len = len(substr)
