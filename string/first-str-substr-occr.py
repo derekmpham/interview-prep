4851e57059e496b311fb68fc566e47e3d76745fb
Derek Pham <derekmpham@gmail.com>
2017-11-19 17:48:23 -0800

    Debug and add first test case

--- string/first-str-substr-occr.py
+++ string/first-str-substr-occr.py
@@ -3,17 +3,27 @@
 def find_substring(string, substr):
 	string_len = len(string)
 	substr_len = len(substr)
-	i = 0
+	j = 0
 
 	if substr_len >= 1:
 		i = 0
 		while i < string_len:
 			if string[i] == substr[j]:
+				j += 1
 				if j == substr_len:
 					return i - substr_len
-					j += 1
 				else:
 					j = 0
-		i += 1
+			i += 1
+		
 
 	return -1
+
+
+# test cases
+# test 1
+string = "Hello"
+substr = "eo"
+print find_substring(string, substr)
+
+
