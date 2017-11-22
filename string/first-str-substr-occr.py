f11d029c718d81b1f088daa978dc957a16144c32
Derek Pham <derekmpham@gmail.com>
2017-11-22 15:58:34 -0800

    More debugging

--- string/first-str-substr-occr.py
+++ string/first-str-substr-occr.py
@@ -1,20 +1,18 @@
 # Implement a function that takes two strings, s and x, as arguments and finds the first occurrence of the string x in s. The function should return an integer indicating the index in s of the first occurrence of x. If there are no occurrences of x in s, return -1
 
 def find_substring(string, substr):
-	string_len = len(string) # 5
-	substr_len = len(substr) # 2
+	string_len = len(string)
+	substr_len = len(substr)
 	j = 0
 
 	if substr_len >= 1:
-		for i in range(0, string_len - 1):
+		for i in range(0, string_len):
 			if string[i] == substr[j]:
 				if j == substr_len - 1:
 					return i - substr_len + 1
-				# else:
-				# 	j = 0
 				j += 1
-			# else:
-			# 	j = 0
+			else:
+				j = 0
 			i += 1
 
 	return -1
@@ -29,6 +27,11 @@ print find_substring(string, substr) # -1
 # test 2
 string = "Hello"
 substr = "el"
-print find_substring(string, substr) # 1 ---> still need to debug more
+print find_substring(string, substr) # 1
+
+# test 3
+string = "a"
+substr = "a"
+print find_substring(string, substr) # 0
 
 
