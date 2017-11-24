5881f03de3ffeeecca460e71b531f07e1dae7e46
Derek Pham <derekmpham@gmail.com>
2017-11-24 12:39:47 -0800

    Refactor: implement rabin-karp  search algorithm

--- string/first-str-substr-occr.py
+++ string/first-str-substr-occr.py
@@ -1,10 +1,21 @@
 # Implement a function that takes two strings, s and x, as arguments and finds the first occurrence of the string x in s. The function should return an integer indicating the index in s of the first occurrence of x. If there are no occurrences of x in s, return -1
 
 def find_substring(string, substr):
-	if len(substr) >= 1:
-		for i in range(0, len(string)):
-			if string[i:i+len(substr)] == substr:
+	if len(substr) >= 1 and len(string) >= len(substr):
+		substr_hash = 0
+		string_hash = 0
+
+		for j in range(len(substr)):
+			string_hash += ord(string[j])
+			substr_hash += ord(substr[j])
+
+		for i in range(0, len(string) - len(substr) + 1):
+			if string_hash == substr_hash and string[i:i+len(substr)] == substr:
 				return i
+			# rolling hash
+			if i + len(substr) < len(string):
+				string_hash = string_hash - ord(string[i]) + ord(string[i + len(substr)])
+
 	return -1
 
 
@@ -36,5 +47,15 @@ assert find_substring(string, substr) == 1
 
 # test 6
 string = "lrnkbldxguzgcseccinlizyogwqzlifxcthdgmanjztlt"
-substr = "an" # 38
+substr = "an"
 assert find_substring(string, substr) == 38
+
+# test 7
+string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaa"
+substr = "ab"
+assert find_substring(string, substr) == 31
+
+# test 8
+string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+substr = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+assert find_substring(string, substr) == -1
